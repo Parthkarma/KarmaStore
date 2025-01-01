f9524b04090f7b51286e44a2ad9313f641aae88b
parth karma <codeparthk@gmail.com>
2025-01-01 21:15:46 +0530

    saving products to the category

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -6,7 +6,7 @@ import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
-import com.ecommerce.project.repositories.CategoryRepositories;
+import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,7 +15,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 // An Optional is used to represent a value that might or might not be present, making it a safe way to handle potential null values.
 @Service
@@ -26,7 +25,7 @@ public class CategoryServiceImpl implements CategoryService{
 //    private List<Category> categories = new ArrayList<>();
 //    private Long nextId = 1L;
     @Autowired
-    private CategoryRepositories categoryRepository;
+    private CategoryRepository categoryRepository;
     @Autowired
     private ModelMapper modelMapper ;
     @Override
---
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.service;
+
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ProductService {
+
+
+ ProductDTO addProduct(Long categoryId, Product product);
+}
